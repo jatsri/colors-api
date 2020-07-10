2e0414adebd7fda4ada925854b07ffae6b5baa30
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-07-10 10:22:53 +0200

    Create /colors endpoint

---
+++ colors.js
@@ -0,0 +1,18 @@
+const colors = {
+    items: [
+        {
+            id: 'red',
+            hex: '#FF0000'
+        },
+        {
+            id: 'deeppink',
+            hex: '#FF1493'
+        },
+        {
+            id: 'orange',
+            hex: '#FFA500'
+        }
+    ]
+};
+
+module.exports = colors
--- createEndpoints.js
+++ createEndpoints.js
@@ -1,7 +1,13 @@
+const  colors = require('./colors');
+
 const createEndpoints = ({ app }) => {
     app.get('/health', (req, res) => {
         res.send('Healthy')
     });
+
+    app.get('/colors', (req, res) => {
+        res.json(colors);
+    })
 }
 
 module.exports = createEndpoints;
