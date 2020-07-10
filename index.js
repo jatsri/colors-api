298133ffab3012d4c731d464cacb18d542c1f894
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-07-10 14:38:47 +0200

    Add body parser

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const createEndpoints = require('./src/createEndpoints');
 const { query } = require('./src/db/query');
 
 const app = express();
+app.use(bodyParser.json({ type: 'application/json' }));
 
 createEndpoints({
     app,
