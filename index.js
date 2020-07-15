1a49e873b6f8b4e7d53d2488dba45cd1755c64de
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-07-15 22:05:57 +0200

    Add cors middleware

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const createEndpoints = require('./src/createEndpoints');
 const { query, getConnection } = require('./src/db/query');
 
 const app = express();
 app.use(bodyParser.json({ type: 'application/json' }));
+app.use(cors())
 
 createEndpoints({
     app,
