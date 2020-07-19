07e4ec404e7938bb5ff65f36a844ab3b81a3505c
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-07-19 16:10:45 +0200

    Add test case for the get colors endpoint

--- test/createEndpoints.test.js
+++ test/createEndpoints.test.js
@@ -1,12 +1,17 @@
 const { expect } = require('chai');
 const request = require('supertest');
 const express = require('express');
+const sinon  = require('sinon');
 
 const createEndpoints = require('../src/createEndpoints');
 
+const response = { rows: [ { id: 1, name: 'red', hex: '#REDCOL' }]};
+
 const createApp = () => {
     const app = express();
-    createEndpoints({ app });
+    const query = sinon.stub().resolves(response)
+
+    createEndpoints({ app, query });
     return app;
 }
 
@@ -18,5 +23,14 @@ describe('createEndpoints', () => {
 
         expect(res.status).to.equal(200);
         expect(res.text).to.equal('Healthy');
+    });
+
+    it('should return colors on get to /colors', async () => {
+        const app = createApp();
+
+        const res = await request(app).get('/colors');
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal({ items: response.rows });
     });
 });
